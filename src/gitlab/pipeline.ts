edcf23088491278bd594a9b2234580ea87877d50
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-07-05 19:42:23 +0000

    add `bun install` job

--- src/gitlab/pipeline.ts
+++ src/gitlab/pipeline.ts
@@ -1,6 +1,10 @@
 import { GitlabCI } from "https://deno.land/x/fluent_gitlab_ci@v0.3.2/mod.ts";
-import { test } from "./jobs.ts";
+import { install, test } from "./jobs.ts";
 
-const gitlabci = new GitlabCI().image("oven/bun:latest").addJob("test", test);
+const gitlabci = new GitlabCI()
+  .image("oven/bun:latest")
+  .cache(["node_modules/"])
+  .addJob("install", install)
+  .addJob("test", test);
 
 export default gitlabci;
