22f0d4a39bbc96c1f8e779572470fcc1af3f305c
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-02-26 20:30:30 +0000

    fix run function

--- src/dagger/pipeline.ts
+++ src/dagger/pipeline.ts
@@ -1,23 +1,26 @@
-import { uploadContext } from "../../deps.ts";
 import * as jobs from "./jobs.ts";
 
-const { test, runnableJobs, exclude } = jobs;
+const { test, runnableJobs } = jobs;
 
 export default async function pipeline(src = ".", args: string[] = []) {
-  if (Deno.env.has("FLUENTCI_SESSION_ID")) {
-    await uploadContext(src, exclude);
-  }
   if (args.length > 0) {
-    await runSpecificJobs(args as jobs.Job[]);
+    await runSpecificJobs(args);
     return;
   }
 
-  await test();
+  await test(src);
 }
 
-async function runSpecificJobs(args: jobs.Job[]) {
+async function runSpecificJobs(args: string[]) {
+  if (args[0] === "run" && args.length > 1) {
+    const job = runnableJobs["run"];
+    for (const name of args.slice(1)) {
+      await job(name);
+    }
+    return;
+  }
   for (const name of args) {
-    const job = runnableJobs[name];
+    const job = runnableJobs[name as jobs.Job];
     if (!job) {
       await runnableJobs.run(name);
     }
