c7c38f9f86909e6c237092c3955f5e6cb1ce205a
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-04-15 18:17:29 +0000

    [plugin] add setup function

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -2,6 +2,22 @@ use extism_pdk::*;
 use fluentci_pdk::dag;
 
 #[plugin_fn]
+pub fn setup(version: String) -> FnResult<String> {
+    let version = if version.is_empty() {
+        "latest".to_string()
+    } else {
+        version
+    };
+
+    let stdout = dag()
+        .pkgx()?
+        .with_exec(vec!["type node > /dev/null || pkgx install node"])?
+        .with_exec(vec!["pkgx", "install", &format!("bun@{}", version)])?
+        .stdout()?;
+    Ok(stdout)
+}
+
+#[plugin_fn]
 pub fn test(args: String) -> FnResult<String> {
     let stdout = dag()
         .pipeline("test")?
